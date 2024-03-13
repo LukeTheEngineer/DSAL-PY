46b4f324d52a80a0416f245ec438468651e642e1
LukasTheEngineer <LukasJacksonEG@gmail.com>
2024-03-12 19:25:25 -0500

    Added data structures templates for future work.(Currently busy)

--- dsal_py_src/dsa/binarytree.py
+++ dsal_py_src/dsa/binarytree.py
@@ -1,5 +1,5 @@
 """
-File: linkedlist.py
+File: binarytree.py
 Description: Linked list data structures and algorithm manipulation
 
 Date: March 12th, 2024
@@ -34,50 +34,3 @@ License: BSD-3-Clause License
          OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
          OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
-
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-class LinkedList:
-    """ A standard linked list for now"""
-    def __init__(self):
-        self.head = None
-
-    def append(self, data):
-        new_node = Node(data)
-        if self.head is None:
-            self.head = new_node
-            return
-        last_node = self.head
-        while last_node.next:
-            last_node = last_node.next
-        last_node.next = new_node
-
-    def prepend(self, data):
-        new_node = Node(data)
-        new_node.next = self.head
-        self.head = new_node
-
-    def delete(self, data):
-        current_node = self.head
-        if current_node and current_node.data == data:
-            self.head = current_node.next
-            current_node = None
-            return
-        prev_node = None
-        while current_node and current_node.data != data:
-            prev_node = current_node
-            current_node = current_node.next
-        if current_node is None:
-            return
-        prev_node.next = current_node.next
-        current_node = None
-
-    def print_list(self):
-        current_node = self.head
-        while current_node:
-            print(current_node.data, end=" -> ")
-            current_node = current_node.next
-        print("None")
